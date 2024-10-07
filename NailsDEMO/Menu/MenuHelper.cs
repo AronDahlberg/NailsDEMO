5c849e99996869ef4ae7229611ed11ba9efa81d7
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-10-07 16:51:48 +0200

    update

--- NailsDEMO/Menu/MenuHelper.cs
+++ NailsDEMO/Menu/MenuHelper.cs
@@ -2,7 +2,7 @@
 {
     internal static class MenuHelper
     {
-        public static string ClearScreen { get; } = "\x1b[2J\x1b[H"; // ANSI ESC Code
+        public static string ClearScreen { get; } = "\x1b[m\x1b[2J\x1b[H"; // ANSI ESC Code
 
         public static string? GetUserInput()
         {
